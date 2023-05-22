ce590f638e4e5797f2304aefd44f249b5c41bdf8
vincent van de merwe <335479@student.mboutrecht.nl>
2023-05-22 09:18:15 +0200

    changed the sql create script because i forgot the primary key and forgot to use the database, added that to the script

--- db/contact-database-create-script.sql
+++ db/contact-database-create-script.sql
@@ -1,9 +1,11 @@
 drop database if exists contact;
 create database if not exists contact;
 
+use contact;
+
 drop table if exists contacts;
 create table if not exists contacts (
-id int unsigned not null auto_increment,
+id int unsigned not null auto_increment primary key,
 name varchar(40) not null,
 email varchar(50) not null,
 message varchar(60) not null
